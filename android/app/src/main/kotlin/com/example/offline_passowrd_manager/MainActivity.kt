7bf5211e30313d0596b7fed3ac3e2e96ffd9e3d3
Hayashi19 <55201076+hayashi19@users.noreply.github.com>
2022-04-21 23:29:51 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/offline_passowrd_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.offline_passowrd_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/list_model.dart
@@ -0,0 +1,25 @@
+class Todo {
+  String title;
+  String username;
+  String pw;
+  String note;
+  bool show;
+
+  Todo({required this.title, required this.username, required this.pw, required this.note, required this.show});
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+    title: json['title'],
+    username: json['username'],
+    pw: json['pw'], 
+    note: json['note'], 
+    show: json['show']
+  );
+
+  Map<String, dynamic> toJson() => {
+    'title' : title,
+    'username': username,
+    'pw': pw,
+    'note': note,
+    'show': show
+  };
+}
